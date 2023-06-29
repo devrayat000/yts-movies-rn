c477c1b6951de72742913c206ce51efb719ca4e7
Zul Ikram Musaddik Rayat <2010050@me.buet.ac.bd>
2023-06-29 17:23:59 +0600

    optimized lists

---
+++ src/components/Factory.ts
@@ -0,0 +1,4 @@
+import { Factory } from "native-base";
+import { TouchableHighlight as NormalTouchableHighlight } from "react-native-gesture-handler";
+
+export const TouchableHighlight = Factory(NormalTouchableHighlight);
